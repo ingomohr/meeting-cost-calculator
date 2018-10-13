2c453e19cf506ebc630cffe76f990aa7870c3acc
Ingo Mohr <tellastory73@gmail.com>
2018-10-14 00:59:39 +0200

    #10 output now uses currency separator

--- src/components/OutputActualCostsForMeeting.js
+++ src/components/OutputActualCostsForMeeting.js
@@ -2,14 +2,13 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 
 class OutputActualCostsForMeeting extends React.Component {
-
   render() {
     return (
       <div>
         <TextField
           id="actualCosts"
           label="Actual costs ($)"
-          type="number"
+          type="currency"
           value={this.props.actualCosts}
           InputLabelProps={{
             shrink: true
