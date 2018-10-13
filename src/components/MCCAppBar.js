21811d46af07902aefec8b462069d26b8c541ec1
Ingo Mohr <tellastory73@gmail.com>
2018-10-14 00:19:49 +0200

    #3 remove unused code/imports

--- src/components/MCCAppBar.js
+++ src/components/MCCAppBar.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
-
 class MCCAppBar extends React.Component {
   render() {
-    const { classes } = this.props;
     return (
       <div className={root}>
         <AppBar position="static">
